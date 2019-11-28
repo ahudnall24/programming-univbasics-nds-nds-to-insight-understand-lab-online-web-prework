2ea0e553bf495a76cab42ea10e9eceacbfba2430
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-28 00:33:46 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -3,7 +3,17 @@ require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
-  # Change the code below to pretty print the nds with pp
+  [ # outermost structure is an Array
+  [ #oh, but another one immediately, so it's an AoA
+    [ # yet another!? It's an AoAoA where the inner Arrays are full of ..
+      {:name=>"Vanilla Cookies", :price=>3}, # Hashes with two keys!
+      {:name=>"Pistachio Cookies", :price=>3}, # and another Hash
+      {:name=>"Chocolate Cookies", :price=>3}, # and another Hash
+      {:name=>"Chocolate Chip Cookies", :price=>3} # and another Hash
+    ], # end of inner array
+    [ #...and so on...
+      {:name=>"Tooth-Melters", :price=>12},
+     {:name=>"Tooth-Destroyers", :price=>12},# Change the code below to pretty print the nds with pp
   nil
 end
 
