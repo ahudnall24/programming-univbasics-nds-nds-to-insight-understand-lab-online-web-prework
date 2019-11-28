24545d62cf5c38ae8cd3102a15dfd467234890d1
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-28 00:37:54 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
-# Call the method directors_database to retrieve the NDS
+vm=[:movie_title=>"The Departed",# Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
     
